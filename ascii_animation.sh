02aae0f55e6886f45581f0a0118524f0f12b2aaa
Arthur Muller <arty97muller@gmail.com>
2025-05-20 16:24:03 +0200

    Implement new frame wait logic + Debug utils
    Alternative wait logic for frame times, old sleep one is just commented
    out.
    This has been implemented in hopes of fixis a current bug, where the video playback
    is delayed in case of videos at 60fps with many frams but speed up in the case
    of gifs at 25/30 fps with less frames. Some utils have been created to help with
    debugging the issue but little progress has been made.
    Ideally having the frame gen in the loop isn't great, should instead pre-process
    the frames in a buffer queue and then print the frames at playback separating
    rendering and playback but for sake of simplicity this is the way for now.
    Maybe bash isn't the best choice for this setup but for a simply proof of concept
    this will have to do.
    the new rendering system basically waits for a frame time to arrive and only if the
    time is correct will print it but still it doesn't seem to be behaving correctly,
    maybe its the loop fault or it could simply be the hoverhead of having to perform
    all these action and have the video printed by that time.

--- ascii_animation.sh
+++ ascii_animation.sh
@@ -2,29 +2,39 @@
 
 # Add --chars="$CHARS" flag to jp2a to only use the following characters
 CHARS=".:!_-~=MAL0O"
-DEFAULT_JP2A_OPTIONS=(--color-depth=24 --width=150)
-
-FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 $1 | bc -l)
-frame_time=$(bc --expression="scale = 10; 1 / ${FPS}")
+DEFAULT_JP2A_OPTIONS=(--color-depth=24 --width=80)
 
 ffmpeg -i $1 -v error -vf scale=320:-1 frames/frame_%03d.png
 
+# Directly extrapolate FPS from number of frames and video lenght for debugging
+#
+# number_of_frames=$(ls frames/*.png | wc -l)
+video_lenght=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $1)
+# FPS=$(bc --expression="scale = 6; ${number_of_frames} / ${video_lenght}")
+
+FPS=$(ffprobe -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 $1 | bc -l)
+frame_time=$(bc --expression="scale = 6; 1 / ${FPS}") # could be an issue here with the conversion from s/ms
+
 precise_sleep() {
     local ms=$1
-    local sec=$(echo "scale=6; $ms/1000" | bc)
+    local sec=$(echo "scale=6; $ms/1000" | bc) #or here dedicing if s or ms should be used
     
     # Use perl for microsecond precision sleep
     # This has less overhead than bash's built-in sleep
     perl -e "select(undef, undef, undef, $sec);"
 }
 
-start_video_buffer(){
+get_time() {
+    perl -MTime::HiRes=time -e 'printf("%.9f\n", time)'
+}
+
+start_video_buffer() {
     clear
     tput civis
     echo -e "\033[?1049h"
 }
 
-end_video_buffer(){
+end_video_buffer() {
     echo -e "\033[?1049l"
     tput cnorm
     clear
@@ -32,11 +42,30 @@ end_video_buffer(){
 }
 
 start_video_buffer
+start_time=$(get_time)
+i=0
+
 for frame in frames/frame_*.png; do
     # Unccoment for debbuggin purpose
     # echo "FPS:[${FPS}] Frame Time:[${frame_time}]" 
+    # finish_time=$(get_time)
+    # echo "FPS:[${FPS}] Frame Time:[${frame_time}] Elapsed:[$(echo "$finish_time - $start_time" | bc -l)] Original lenght[${video_lenght}]"
+
+    target_time=$(echo "$start_time + $i * $frame_time" | bc -l)
+
     output=$(jp2a "${DEFAULT_JP2A_OPTIONS[@]}" "$frame")
     echo -e "\033[H\033[J${output}"
-    precise_sleep $frame_time
+    # precise_sleep $frame_time
+
+    while :; do
+        now=$(get_time)
+        is_ready=$(echo "$now >= $target_time" | bc)
+        [[ is_ready -eq 1 ]] && break
+        precise_sleep 0,001
+    done
+    i=$((i++))
 done
 end_video_buffer
+
+finish_time=$(get_time)
+echo "FPS:[${FPS}] Frame Time:[${frame_time}] Elapsed:[$(echo "$finish_time - $start_time" | bc -l)] Original lenght[${video_lenght}]"
