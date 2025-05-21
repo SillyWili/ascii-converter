6c859aeb1d97c97f3bf428aafbc0f34f0eb8dbaa
Arthur Muller <arty97muller@gmail.com>
2025-05-22 01:56:58 +0200

    Add ascii-display.c basic logic
    the algorithm does the same base thing of ascii-animation.sh but now in C!
    It allows for some better manipulation of the logic even if a bit more
    complicated in logic and syntax.
    At its core the algo first runs ffmpeg to disect the input video into
    its frames as png - see frame-generator.sh - converts each frame to
    a ascii escape sequence and puts it into the frames.txt file.
    the c part just reads each frame at a time, prints it, waits for a
    set frame-time (here its manual for now, will try to implement a self
    detecting system) clears the screen and repeats untill the file is no more.
    everything is suboptimal for now, will have to improve, but its a good proof
    of concept for now :)

---
+++ ascii-display.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+const char *CLEAR_SCREEN = "\033[2J\033[H";
+const char *HIDE_CURSOR = "\033[?25l";
+const char *SHOW_CURSOR = "\033[?25h";
+const char *OPEN_BUFFER = "\033[?1049h";
+const char *CLOSE_BUFFER = "\033[?1049l";
+
+void clear_screen() { printf("%s", CLEAR_SCREEN); }
+void open_buffer() { printf("%s", OPEN_BUFFER); }
+void close_buffer() { printf("%s", CLOSE_BUFFER); }
+void show_cursor() { printf("%s", SHOW_CURSOR); }
+void hide_cursor() { printf("%s", HIDE_CURSOR); }
+
+int main(int argc, char const *argv[]) {
+
+    if (argc == 1) {
+        perror("No input file passed as argument for the script.\nPlease "
+               "provide a video as input for the script to work\n");
+        return 1;
+    } else if (argc > 2) {
+        printf("WARNING: more than one argument as been passed as input to the "
+               "script, only the first will be displayed.\n");
+    }
+
+    char script_name[100] = "./ascii-frame-generator.sh ";
+    char file_name[100];
+    strcpy(file_name, argv[1]);
+
+    printf("\nProcessing the frames\n");
+    system(strcat(script_name, file_name));
+    clear_screen();
+
+    // main display logic for the program
+    hide_cursor();
+    open_buffer();
+
+    FILE *fptr = fopen("frames.txt", "r");
+    if (fptr == NULL) {
+        perror("Failed to open the frames.txt file\n");
+        return 1;
+    }
+
+    while (1) {
+        int lines_read = 0;
+        char line[4096];
+        while (fgets(line, sizeof(line), fptr) != NULL) {
+            if (line[0] == '\n') {
+                break;
+            }
+
+            fputs(line, stdout);
+            fflush(stdout);
+            lines_read++;
+        }
+
+        usleep(40 * 1000);
+        clear_screen();
+
+        if (lines_read == 0) {
+            break;
+        }
+    }
+
+    fclose(fptr);
+    close_buffer();
+    show_cursor();
+
+    return 0;
+}
